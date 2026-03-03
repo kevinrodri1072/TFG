af44ee829c2c3b5a8f00767718b7681a3488ecef
Kevin <kevinrodri1072@gmail.com>
2026-03-03 01:40:39 +0100

    Versió en la qual podem afegir hosts en temps real des de la interfície gràfica web.

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, jsonify
-from xarxa import nodes, matriu_xarxa
+from flask import Flask, render_template, jsonify, request
+import xarxa
+import threading
+import time
 
 app = Flask(__name__)
+
+
 
 @app.route('/')
 def index():
@@ -9,13 +13,55 @@ def index():
 
 @app.route('/topologia')
 def topologia():
-    noms_nodes = list(nodes.keys())
+    noms_nodes = list(xarxa.nodes.keys())
     links = []
-    for i in range(len(matriu_xarxa)):
-        for j in range(i+1, len(matriu_xarxa)):
-            if matriu_xarxa[i][j] == 1:
+    for i in range(len(xarxa.matriu_xarxa)):
+        for j in range(i+1, len(xarxa.matriu_xarxa)):
+            if xarxa.matriu_xarxa[i][j] == 1:
                 links.append({'from': noms_nodes[i], 'to': noms_nodes[j]})
-    return jsonify({'nodes': nodes, 'links': links})
+    return jsonify({'nodes': xarxa.nodes, 'links': links})
+
+@app.route('/afegir_host', methods=['POST'])
+def afegir_host():
+    if not xarxa.xarxa_llesta:
+        return jsonify({'ok': False, 'error': 'Xarxa no llesta'})
+    
+    dades = request.json
+    nom = dades['nom']
+    switch = dades['switch']
+    
+    # Calculem IP i gateway automàticament
+    router = xarxa.trobar_router_del_switch(switch)
+    ip = xarxa.trobar_seguent_ip(router)
+    gw = xarxa.nodes[router]['ips']['eth1'].split('/')[0]
+    
+    # Afegim al diccionari de nodes
+    xarxa.actualitzar_matriu(nom, switch)
+    xarxa.nodes[nom] = {'tipus': 'host', 'ip': ip, 'gw': gw}
+    
+    # Afegim a Mininet
+    nou_host = xarxa.net.addHost(nom, ip=ip)
+    xarxa.mininet_nodes[nom] = nou_host
+    
+    # Calculem el nom de la nova interfície del switch
+    sw_node = xarxa.mininet_nodes[switch]
+    num_intfs = len(sw_node.intfList())
+    sw_intf_name = f'{switch}-eth{num_intfs}'
+    
+    # Connectem al switch
+    xarxa.net.addLink(nou_host, sw_node,
+                      intfName1=f'{nom}-eth0',
+                      intfName2=sw_intf_name)
+    
+    # Activem les interfícies
+    nou_host.cmd(f'ifconfig {nom}-eth0 {ip}')
+    nou_host.cmd(f'ip route add default via {gw}')
+    
+    return jsonify({'ok': True})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    t = threading.Thread(target=xarxa.iniciar_xarxa)
+    t.daemon = True
+    t.start()
+    time.sleep(3)  # Esperem que Mininet arrenqui
+    app.run(debug=False)
